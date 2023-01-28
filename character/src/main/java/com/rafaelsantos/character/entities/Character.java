586e37e077373a4dab684e48fabfce6dfb4e5858
Rafael Santos <rafa.05125@gmail.com>
2023-01-28 15:44:36 -0300

    Character

---
+++ character/src/main/java/com/rafaelsantos/character/entities/Character.java
@@ -0,0 +1,60 @@
+package com.rafaelsantos.character.entities;
+
+import java.util.Objects;
+
+public class Character {
+	
+	private Long id;
+	private String name;
+	private String world;
+	
+	public Character() {}
+	
+	public Character(Long id, String name, String world) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.world = world;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getWorld() {
+		return world;
+	}
+
+	public void setWorld(String world) {
+		this.world = world;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Character other = (Character) obj;
+		return Objects.equals(id, other.id);
+	}
+}
